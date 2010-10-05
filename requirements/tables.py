b0167b919c60e4daf1078d866796829c07b79cd1
seba <sebastian@binop.com>
2010-10-05 16:46:39 +0200

    tables for filters included

---
+++ requirements/tables.py
@@ -0,0 +1,14 @@
+import django_tables as tables
+from django.utils.safestring import mark_safe
+from tcstorm_requirements.requirements.models import Requirement
+
+# test = lambda x,y: 4
+
+class RequirementsFilterTable(tables.ModelTable):
+    checked = tables.Column(verbose_name=" ")
+
+    class Meta:
+        model = Requirement
+
+    def render_checked(self, a):
+        return mark_safe("<input type='checkbox' />")
