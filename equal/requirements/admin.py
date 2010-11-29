e0b04bd49e880ee0731bec8efc347aab27b8b925
seba <sebastian.pawlus@binop.com>
2010-11-29 10:32:59 +0100

    styles for form errors validation

--- equal/requirements/admin.py
+++ equal/requirements/admin.py
@@ -5,6 +5,5 @@ from mptt.admin import MPTTModelAdmin
 from equal.requirements.models import Requirement
 
 class DirectoryAdmin(MPTTModelAdmin):
-    exclude = ("dependency_root",)
     list_display = ('name', 'modified_time', "created_time")
 admin.site.register(Requirement, DirectoryAdmin)
