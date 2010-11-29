3a3770a187b5c98645bae35c369d85e92762df63
seba <sebastian.pawlus@binop.com>
2010-11-29 16:13:47 +0100

    Dependency fields added

--- equal/requirements/forms.py
+++ equal/requirements/forms.py
@@ -2,7 +2,9 @@ from django import forms
 from equal.requirements.models import Requirement
 
 class RequirementForm(forms.ModelForm):
+    dependency = forms.ModelMultipleChoiceField(queryset=Requirement.objects.all())
+    
     class Meta:
         model = Requirement
-        fields = ("name","parent","release_target","description")
+        fields = ("name", "parent", "release_target", "dependency", "description")
         widgets = {'release_target': forms.DateInput(attrs={'class':'date-field'})}
