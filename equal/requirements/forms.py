741418ac0dfc35690aff3f456967f31feda313fc
seba <sebastian.pawlus@binop.com>
2010-12-06 15:04:51 +0100

    fields sequence changed

--- equal/requirements/forms.py
+++ equal/requirements/forms.py
@@ -2,9 +2,9 @@ from django import forms
 from equal.requirements.models import Requirement
 
 class RequirementForm(forms.ModelForm):
-    dependency = forms.ModelMultipleChoiceField(queryset=Requirement.objects.all())
+    dependency = forms.ModelMultipleChoiceField(queryset=Requirement.objects.all(), required=False)
     
     class Meta:
         model = Requirement
-        fields = ("name", "parent", "release_target", "dependency", "description")
+        fields = ("name", "parent", "release_target", "description", "dependency")
         widgets = {'release_target': forms.DateInput(attrs={'class':'date-field'})}
