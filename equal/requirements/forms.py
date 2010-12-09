daeda4aad04456c62a350365e69afb1773085348
seba <sebastian.pawlus@binop.com>
2010-12-09 16:45:09 +0100

    removed constructor

--- equal/requirements/forms.py
+++ equal/requirements/forms.py
@@ -8,8 +8,3 @@ class RequirementForm(forms.ModelForm):
         model = Requirement
         fields = ("parent", "name", "release_target", "description", "dependencies" )
         widgets = {"release_target": forms.DateInput(attrs={"class":"date-field"})}
-
-    def __init__(self, *args, **kwargs):
-        super(RequirementForm, self).__init__(*args, **kwargs)
-        if "initial" in kwargs: 
-            self.fields["dependencies"].queryset = Requirement.objects.filter(pk=1)
