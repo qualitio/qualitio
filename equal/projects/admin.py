0fa15d90155c90b9252e2e31f80c0d7a3c9383e7
seba <sebastian.pawlus@binop.com>
2011-01-03 16:23:21 +0100

    Project renamed from equal -> qualitio

--- equal/projects/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from treebeard.admin import TreeAdmin
-from equal.projects.models import *
-
-admin.site.register(Team)
---
+++ qualitio/requirements/admin.py
@@ -0,0 +1,8 @@
+from mptt.admin import MPTTModelAdmin
+from django.contrib import admin
+from qualitio.requirements.models import Requirement
+
+class RequirementAdmin(MPTTModelAdmin):
+    list_display = ('name', 'modified_time', "created_time")
+    readonly_fields = ('path',)
+admin.site.register(Requirement, RequirementAdmin)
