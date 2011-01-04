b5f3798eb6b2ad6bde107053373afc85394145ad
xando <sebastian.pawlus@qualitio.com>
2011-01-04 14:44:15 +0100

    Fields types fixes

--- qualitio/requirements/models.py
+++ qualitio/requirements/models.py
@@ -4,9 +4,9 @@ from qualitio.core import models as core
 
 class Requirement(core.BaseDirectoryModel):
     description = models.TextField(blank=True)
-    alias = models.TextField(blank=True) #TODO: alias is not unique and should be moved to core.models
-    release_target = models.DateField(null=True)
-    dependencies = models.ManyToManyField("Requirement", related_name="blocks", null=True)
+    alias = models.CharField(blank=True, max_length=512) #TODO: alias is not unique and should be moved to core.models
+    release_target = models.DateField(blank=True, null=True)
+    dependencies = models.ManyToManyField("Requirement", related_name="blocks", null=True, blank=True)
     
     def get_absolute_url(self):
         return "/require/" % self.id
