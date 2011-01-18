bcd2319719628cf94782253ba963a0df4ca38ade
zulawin <jacek.zulawinski@binop.com>
2011-01-18 16:21:07 +0100

    Added new test

--- tests/require/browse/tests.py
+++ tests/require/browse/tests.py
@@ -2188,5 +2188,175 @@ class Test17StoreTestdirectCreate(BaseSeleniumTestCase):
         except AssertionError, e: self.verificationErrors.append(str(e))
 
 
+class Test18StoreTestcaseCreate(BaseSeleniumTestCase):
+    
+    def test_18_store_testcase_create(self):
+        sel = self.selenium
+        sel.open("/require/#requirement/1/details/")
+        try: self.assertEqual("qualitio: requirements", sel.get_title())
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        for i in range(60):
+            try:
+                if "store" == sel.get_text("link=store"): break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+        sel.click("link=store")
+        sel.wait_for_page_to_load("30000")
+        for i in range(60):
+            try:
+                if sel.is_text_present("MeeGo Netbook"): break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+        try: self.assertEqual("qualitio: store", sel.get_title())
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        sel.click("link=MeeGo Netbook")
+        for i in range(60):
+            try:
+                if sel.is_text_present("create testcase"): break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+        try: self.failUnless(sel.is_text_present("create testcase"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        sel.click("css=div#application-view-footer div a")
+        for i in range(60):
+            try:
+                if sel.is_text_present("test case"): break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+        sel.select_window("null")
+        try: self.failUnless(sel.is_text_present("test case"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        try: self.failUnless(sel.is_element_present("//div[@id='application-view-menu']/span"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        try: self.assertEqual("new", sel.get_text("//div[@id='application-view-menu']/span"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        try: self.failUnless(sel.is_text_present("Parent"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        try: self.failUnless(sel.is_element_present("id_parent"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        sel.select("id_parent", "label=MeeGo Handset bat")
+        try: self.failUnless(sel.is_element_present("id_name"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        sel.type("id_name", "test case 1")
+        try: self.failUnless(sel.is_element_present("id_requirement"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        sel.select("id_requirement", "label=/MeeGo")
+        for i in range(60):
+            try:
+                if sel.is_element_present("id_description"): break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+        sel.type("id_description", "desription")
+        try: self.failUnless(sel.is_element_present("id_precondition"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        sel.type("id_precondition", "precondition")
+        try: self.failUnless(sel.is_element_present("//a[@id='add-step-0']/span"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        try: self.assertEqual("add step", sel.get_text("//a[@id='add-step-0']/span"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        sel.click("//a[@id='add-step-0']/span")
+        for i in range(60):
+            try:
+                if sel.is_text_present("Step 1"): break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+        try: self.failUnless(sel.is_element_present("id_testcasestep_set-0-description"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        try: self.failUnless(sel.is_element_present("id_testcasestep_set-0-expected"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        sel.type("id_testcasestep_set-0-description", "Description 1")
+        sel.type("id_testcasestep_set-0-expected", "Expected 1")
+        try: self.failUnless(sel.is_element_present("//form[@id='testcase_form']/div[2]/div[6]/div[1]/a/span"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        sel.click("//form[@id='testcase_form']/div[2]/div[6]/div[1]/a/span")
+        for i in range(60):
+            try:
+                if sel.is_text_present("Step 2"): break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+        try: self.failUnless(sel.is_element_present("id_testcasestep_set-1-description"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        try: self.failUnless(sel.is_element_present("id_testcasestep_set-1-expected"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        sel.type("id_testcasestep_set-1-description", "Description 2")
+        sel.type("id_testcasestep_set-1-expected", "Expected 2")
+        try: self.failUnless(sel.is_element_present("//input[@name='Executed' and @value='Save']"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        sel.click("//input[@name='Executed' and @value='Save']")
+        for i in range(60):
+            try:
+                if sel.is_element_present("link=MeeGo Netbook"): break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+        for i in range(60):
+            try:
+                if sel.is_text_present("MeeGo Netbook"): break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+        for i in range(60):
+            try:
+                if sel.is_text_present("MeeGo Handset bat"): break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+        sel.click("link=MeeGo Handset bat")
+        for i in range(60):
+            try:
+                if sel.is_text_present("test case directory: MeeGo Handset bat"): break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+        for i in range(60):
+            try:
+                if sel.is_element_present("link=test case 1"): break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+        try: self.failUnless(sel.is_element_present("link=test case 1"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        sel.click("//li[@id='2_testcasedirectory']/ins")
+        for i in range(60):
+            try:
+                if "test case 1" == sel.get_text("link=test case 1"): break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+        try: self.assertEqual("test case 1", sel.get_text("link=test case 1"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        sel.click("link=test case 1")
+        for i in range(60):
+            try:
+                if sel.is_text_present("test case: test case 1"): break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+        try: self.failUnless(sel.is_text_present("exact:directory:\n/MeeGo Handset bat/ status:\nempty description desription precondition precondition step 1 Description 1 \n Expected 1 \n \n step 2 Description 2 \n Expected 2"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        sel.click("//div[@id='application-view']/div[3]")
+        try: self.failUnless(sel.is_text_present("exact:directory:\n/MeeGo Handset bat/ status:\nempty description desription precondition precondition step 1 Description 1 \n Expected 1 \n \n step 2 Description 2 \n Expected 2"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        try: self.failUnless(sel.is_text_present("step 1"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        try: self.failUnless(sel.is_text_present("step 2"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        try: self.failUnless(sel.is_text_present("Description 1"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        try: self.failUnless(sel.is_text_present("Expected 1"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        try: self.failUnless(sel.is_text_present("Description 2"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        try: self.failUnless(sel.is_text_present("Expected 2"))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+
+
 if __name__ == "__main__":
     unittest.main()
