c122ab6b6e44c1fc48954e0c4beca9ae0f2741be
xando (sebastian.pawlus@qualitio.com) <sebastian.pawlus@qualitio.com>
2011-01-21 14:51:31 +0100

    models need to be based on core.BaseModels

--- qualitio/execute/models.py
+++ qualitio/execute/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from mptt.models import MPTTModel
 
+### TODO: switch with models to BaseModels
 class TestRunDirectory(MPTTModel):
     parent = models.ForeignKey('self', null=True, blank=True)
 
@@ -23,11 +24,29 @@ class TestRun(models.Model):
     parent = models.ForeignKey('TestRunDirectory')
 
     name = models.CharField(max_length=512)
+    notes = models.TextField(blank=True)
+
 
 class TestCaseRun(models.Model):
+    parent = models.ForeignKey('TestRun', null=True, blank=True)
+    requirement = models.ForeignKey('requirements.Requirement', null=True, blank=True)
+    
+    description = models.TextField(blank=True)
+    precondition = models.TextField(blank=True)
+    
     name = models.CharField(max_length=512)
-    testrun = models.ForeignKey('TestRun')
+        
+    def __unicode__(self):
+        return self.name
 
+class TestCaseStep(core.BaseModel):
+    testcase = models.ForeignKey('TestCase')
+    description = models.TextField()
+    expected = models.TextField(blank=True)
+    sequence = models.PositiveIntegerField(null=True, default=0)
+    
+    class Meta:
+        ordering = ['sequence']
 
 class Status(models.Model):
     name = models.CharField(max_length=512)
