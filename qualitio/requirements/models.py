fa12d3c046a3dd368323e0ddd6ecefeebd7d2259
xando (sebastian.pawlus@qualitio.com) <sebastian.pawlus@qualitio.com>
2011-01-25 16:12:07 +0100

    Methods unused

--- qualitio/requirements/models.py
+++ qualitio/requirements/models.py
@@ -6,9 +6,3 @@ class Requirement(core.BaseDirectoryModel):
     dependencies = models.ManyToManyField("Requirement", related_name="blocks", null=True, blank=True)
     release_target = models.DateField(blank=True, null=True)
     description = models.TextField(blank=True)
-    
-    def get_absolute_url(self):
-        return "/require/" % self.id
-    
-    def __unicode__(self):
-        return "%s%s" % (self.path, self.name)
