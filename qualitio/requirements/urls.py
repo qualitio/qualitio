12e461ff5ef6d67d9c4d5ee5b24f4f9cd325583b
xando (sebastian.pawlus@qualitio.com) <sebastian.pawlus@qualitio.com>
2011-01-27 16:37:00 +0100

    whitespaces

--- qualitio/requirements/urls.py
+++ qualitio/requirements/urls.py
@@ -3,13 +3,13 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('qualitio.requirements.views',
                        url(r'^$', 'index'),
                        url(r'filter/?$', filter),
-                       
+
                        url(r'^ajax/get_children/$', 'get_children'),
                        url(r'^ajax/requirement/(?P<requirement_id>\d+)/details/$', 'details'),
                        url(r'^ajax/requirement/(?P<requirement_id>\d+)/edit/$', 'edit'),
                        url(r'^ajax/requirement/(?P<requirement_id>\d+)/testcases/$', 'test_cases'),
                        url(r'^ajax/requirement/(?P<requirement_id>\d+)/new/$', 'new'),
-                       
+
                        #helpers view, subviews
                        url(r'^ajax/requirement/new/valid/$', 'valid'),
                        url(r'^ajax/requirement/(?P<requirement_id>\d+)/edit/valid/$', 'valid'),
