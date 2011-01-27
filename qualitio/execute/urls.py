39799d0e80b49a8f8981664d38e20ba0ab18ec16
xando (sebastian.pawlus@qualitio.com) <sebastian.pawlus@qualitio.com>
2011-01-27 16:17:42 +0100

    helper views moved to core module

--- qualitio/execute/urls.py
+++ qualitio/execute/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls.defaults import *
 
 from qualitio.execute.views import *
+from qualitio.execute.models import TestRunDirectory
+from qualitio.core.views import get_children
 
 urlpatterns = patterns('',
                        url(r'^$', index),
 
-                       url(r'^ajax/get_children$', get_children),
+                       url(r'^ajax/get_children$', get_children, {'directory': TestRunDirectory}),
                        url(r'^ajax/testrundirectory/(?P<directory_id>\d+)/details/?$', directory_details),
                        url(r'^ajax/testrundirectory/(?P<directory_id>\d+)/new/?$', directory_new),
                        url(r'^ajax/testrundirectory/(?P<directory_id>\d+)/edit/?$', directory_edit),
@@ -13,12 +15,12 @@ urlpatterns = patterns('',
                        # valid
                        url(r'^ajax/testrundirectory/(?P<directory_id>\d+)/edit/valid/?$', directory_valid),
                        url(r'^ajax/testrundirectory/new/valid/?$', directory_valid),
-                       
+
                        url(r'^ajax/testrun/(?P<testrun_id>\d+)/details/?$', testrun_details),
                        url(r'^ajax/testrun/(?P<testrun_id>\d+)/notes/?$', testrun_notes),
                        url(r'^ajax/testrun/(?P<testrun_id>\d+)/edit/?$', testrun_edit),
                        url(r'^ajax/testrun/(?P<directory_id>\d+)/new/?$', testrun_new),
-                       url(r'^ajax/testrun/(?P<testrun_id>\d+)/execute/?$',testrun_execute),
+                       url(r'^ajax/testrun/(?P<testrun_id>\d+)/execute/?$', testrun_execute),
 
                        # valid
                        url(r'^ajax/testrun/(?P<testrun_id>\d+)/edit/valid/?$', testrun_valid),
--- qualitio/store/views.py
+++ qualitio/store/views.py
@@ -1,32 +1,32 @@
-from mptt.models import MPTTModel
-
 from django.views.generic.simple import direct_to_template
-from django.core.exceptions import ObjectDoesNotExist
-from django.utils import simplejson as json
-from django.http import HttpResponse
 
 from qualitio.core.utils import json_response, success, failed
 from qualitio.store.models import TestCaseDirectory, TestCase
 from qualitio.store.forms import TestCaseForm, TestCaseDirectoryForm, AttachmentFormSet, TestCaseStepFormSet, GlossaryWord
 
+
 def index(request):
     return direct_to_template(request, 'store/base.html', {})
 
+
 def directory_details(request, directory_id):
     return direct_to_template(request, 'store/testcasedirectory_details.html',
-                              {'directory' : TestCaseDirectory.objects.get(pk=directory_id)})
+                              {'directory': TestCaseDirectory.objects.get(pk=directory_id)})
+
 
 def directory_edit(request, directory_id):
     directory = TestCaseDirectory.objects.get(pk=directory_id)
     testcasedirectory_form = TestCaseDirectoryForm(instance=directory)
     return direct_to_template(request, 'store/testcasedirectory_edit.html',
-                              { 'testcasedirectory_form' : testcasedirectory_form })
+                              {'testcasedirectory_form': testcasedirectory_form})
+
 
 def directory_new(request, directory_id):
     directory = TestCaseDirectory.objects.get(pk=directory_id)
-    testcasedirectory_form = TestCaseDirectoryForm(initial={'parent': directory })
+    testcasedirectory_form = TestCaseDirectoryForm(initial={'parent': directory})
     return direct_to_template(request, 'store/testcasedirectory_edit.html',
-                              { 'testcasedirectory_form' : testcasedirectory_form })
+                              {'testcasedirectory_form': testcasedirectory_form})
+
 
 @json_response
 def directory_valid(request, directory_id=0):
@@ -39,15 +39,17 @@ def directory_valid(request, directory_id=0):
     if testcase_directory_form.is_valid():
         testcase_directory = testcase_directory_form.save()
         return success(message='testcase directory saved',
-                       data={ "parent_id" : getattr(testcase_directory.parent,"id", 0),
-                              "current_id" : testcase_directory.id })
+                       data={"parent_id": getattr(testcase_directory.parent, "id", 0),
+                              "current_id": testcase_directory.id})
     else:
         return failed(message="Validation errors",
                       data=[(k, v[0]) for k, v in testcase_directory_form.errors.items()])
 
+
 def testcase_details(request, testcase_id):
     return direct_to_template(request, 'store/testcase_details.html',
-                              {'testcase' : TestCase.objects.get(pk=testcase_id)})
+                              {'testcase': TestCase.objects.get(pk=testcase_id)})
+
 
 def testcase_edit(request, testcase_id):
     testcase = TestCase.objects.get(pk=testcase_id)
@@ -55,9 +57,9 @@ def testcase_edit(request, testcase_id):
     testcasesteps_form = TestCaseStepFormSet(instance=testcase)
     glossary_word_search_form = GlossaryWord()
     return direct_to_template(request, 'store/testcase_edit.html',
-                              { 'testcase_form' : testcase_form,
-                                'testcasesteps_form' : testcasesteps_form,
-                                'glossary_word_search_form' : glossary_word_search_form })
+                              {'testcase_form': testcase_form,
+                                'testcasesteps_form': testcasesteps_form,
+                                'glossary_word_search_form': glossary_word_search_form})
 
 
 def testcase_new(request, directory_id):
@@ -65,8 +67,8 @@ def testcase_new(request, directory_id):
     testcase_form = TestCaseForm(initial={'parent': directory})
     testcasesteps_form = TestCaseStepFormSet()
     return direct_to_template(request, 'store/testcase_edit.html',
-                              { "testcase_form" : testcase_form,
-                                "testcasesteps_form" : testcasesteps_form })
+                              {"testcase_form": testcase_form,
+                                "testcasesteps_form": testcasesteps_form})
 
 
 @json_response
@@ -84,13 +86,13 @@ def testcase_valid(request, testcase_id=0):
         testcasesteps_form.instance = testcase
         testcasesteps_form.save()
         return success(message='TestCase saved',
-                       data={ "parent_id" : getattr(testcase.parent,"id", 0),
-                              "current_id" : testcase.id })
+                       data={"parent_id": getattr(testcase.parent, "id", 0),
+                              "current_id": testcase.id})
     else:
         formset_errors = []
         for i, error in filter(lambda x: x[1], list(enumerate(testcasesteps_form.errors))):
             for v, k in error.items():
-                formset_errors.append(map(lambda x:(("testcasestep_set-%s-%s") % (i,v) ,x), k)[0])
+                formset_errors.append(map(lambda x: (("testcasestep_set-%s-%s") % (i, v), x), k)[0])
 
         return failed(message="Validation errors",
                       data=[(k, v[0]) for k, v in testcase_form.errors.items()] + formset_errors)
@@ -100,31 +102,5 @@ def testcase_attachments(request, testcase_id):
     testcase = TestCase.objects.get(pk=testcase_id)
     attachments_form = AttachmentFormSet(instance=testcase)
     return direct_to_template(request, 'store/testcase_attachments.html',
-                              {'testcase' : testcase,
-                               'attachments_form' : attachments_form})
-
-def to_tree_element(object, type):
-    state = "closed" if isinstance(object, MPTTModel) else ""
-    return { 'attr' : {'id' : "%s_%s" % (object.pk, type),
-                       'rel' : type},
-             'data' : object.name,
-             'state' : state }
-
-
-def get_children(request):
-    data = []
-
-    try:
-        node_id = int(request.GET.get('id', 0))
-        node = TestCaseDirectory.objects.get(pk=node_id)
-        directories = node.children.all()
-        files = node.subchildren.all()
-
-    except (ObjectDoesNotExist, ValueError):
-        directories = TestCaseDirectory.tree.root_nodes()
-        files = TestCase.objects.filter(parent=None)
-
-    data = map(lambda x: to_tree_element(x, x._meta.module_name), directories)+\
-        map(lambda x: to_tree_element(x,x._meta.module_name), files)
-
-    return HttpResponse(json.dumps(data), mimetype="application/json")
+                              {'testcase': testcase,
+                               'attachments_form': attachments_form})
