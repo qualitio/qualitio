bbfa7bacbb36e5c2eed36cc019b671661c846ed3
Kuba Janoszek <kuba.janoszek@gmail.com>
2011-01-31 15:02:12 +0100

    Requirements dependency cycle detection added.

--- qualitio/requirements/forms.py
+++ qualitio/requirements/forms.py
@@ -1,18 +1,61 @@
 from django import forms
-from qualitio.requirements.models import Requirement
+from django.core.exceptions import ValidationError
 
-class RequirementForm(forms.ModelForm):
+from qualitio.requirements.models import Requirement, RequirementDependencyValidator
+
+
+
+class BaseRequirementForm(forms.ModelForm):
+    class Meta:
+        model = Requirement
+
+    def _post_clean(self):
+        # '_post_clean' hook is responsible for model validation on ModelForm.
+        # We need addintional validation of dependencies.
+        super(BaseRequirementForm, self)._post_clean()
+
+        # If 'dependencies' already have errors we shouldn't check it
+        if not 'dependencies' in self._errors:
+            try:
+                self.instance.clean_dependencies(self.cleaned_data['dependencies'])
+            except ValidationError, e:
+                self._update_errors(e.message_dict)
+
+    def save(self, clean_dependencies=False, *args, **kwargs):
+        # The default behaviour of RequirementForm.save cannot invoke
+        # additional check for dependencies cycles on model because it's
+        # already done in '_post_clean' method.
+
+        # Because of the implementation of 'save' procedure in django.forms.models module,
+        # we need to change the 'save' method behaviour on self.instance just for
+        # the moment when this ModelForm instance will invoke it.
+        # TODO: maybe we should discuss this if Proxy for self.instance with
+        #       overriden 'save' method fits better here.
+
+        original_save = None
+
+        if not clean_dependencies and self.instance:
+            def save(*a, **k):
+                original_save(clean_dependencies=False, *a, **k)
+            original_save = self.instance.save
+            self.instance.save = save
+
+        self.instance = super(BaseRequirementForm, self).save(*args, **kwargs)
+
+        # If 'save' method was changed make sure to restore the changes
+        if original_save:
+            self.instance.save = original_save
+
+        return self.instance
+
+
+
+class RequirementForm(BaseRequirementForm):
     def __init__(self, *args, **kwargs):
         super(RequirementForm, self).__init__(*args, **kwargs)
-        self.fields['dependencies'].required = False
         if self.instance:
-            qs = Requirement.objects.all()
-            qs = qs.exclude(pk=self.instance.pk)
-            qs = qs.exclude(dependencies__in=[self.instance])
-            qs = qs.exclude(dependencies__dependencies__in=[self.instance])
-            self.fields['dependencies'].queryset = qs
-        else:
-            self.fields['dependencies'].queryset = Requirement.objects.all()
+            self.fields['dependencies'].queryset = Requirement.objects.exclude_potential_cycles(self.instance)
+        self.fields['dependencies'].required = False
 
 
     class Meta:
--- qualitio/requirements/tests.py
+++ qualitio/requirements/tests.py
@@ -1,8 +1,180 @@
 from nose.tools import *
 
-from qualitio.requirements.models import Requirement, RequirementDependency
+from django.test import TestCase
+from django.core.exceptions import ValidationError
 
-def requirements_save():
-    requirement = Requirement.objects.create(name="req1")
-    requirement_dependency = RequirementDependency.objects.get(root=requirement)
-    assert requirement, requirement_dependency.root
+from qualitio.requirements.models import Requirement
+from qualitio.requirements.forms import RequirementForm
+
+
+
+class DependencyTestCase(TestCase):
+    """
+    Dependency Test Case separate base class which gives us
+    simple, well defined dependencies graph for test cases
+    that checks requirement with dependencies behaviour.
+
+    Graph:
+
+    BigProject -> []
+    MeeGo      -> [BigProject]
+    IVI        -> [MeeGo, TV]
+    TV         -> [MeeGo, Notebook]
+    TV2        -> [MeeGo, Notebook]
+    Notebook   -> [MeeGo]
+    Toster     -> [IVI]
+    Flashlight -> [Toster]
+    """
+    def setUp(self):
+        mgr = Requirement.objects
+
+        self.big_project = mgr.get(name="BigProject") # assumes we've got it in fixtures !
+        self.mee_go = mgr.create(name="MeeGo")
+        self.mee_go_IVI = mgr.create(name="IVI")
+        self.mee_go_TV = mgr.create(name="TV")
+        self.mee_go_TV2 = mgr.create(name="TV2")
+        self.mee_go_Notebook = mgr.create(name="Notebook")
+        self.toster = mgr.create(name="Toster")
+        self.flashlight = mgr.create(name="Flashlight")
+
+        self.mee_go.dependencies.add(self.big_project)
+        self.mee_go.save()
+
+        self.mee_go_IVI.dependencies.add(self.mee_go)
+        self.mee_go_IVI.dependencies.add(self.mee_go_TV)
+        self.mee_go_IVI.save()
+
+        self.mee_go_TV.dependencies.add(self.mee_go)
+        self.mee_go_TV.dependencies.add(self.mee_go_Notebook)
+        self.mee_go_TV.save()
+
+        self.mee_go_TV2.dependencies.add(self.mee_go)
+        self.mee_go_TV2.dependencies.add(self.mee_go_Notebook)
+        self.mee_go_TV2.save()
+
+        self.mee_go_Notebook.dependencies.add(self.mee_go)
+        self.mee_go_Notebook.save()
+
+        self.toster.dependencies.add(self.mee_go_IVI)
+        self.toster.save()
+
+        self.flashlight.dependencies.add(self.toster)
+        self.flashlight.save()
+
+
+
+class RequirementDependenciesOnModelTest(DependencyTestCase):
+    def add_cycle_dependency(self):
+        self.mee_go_Notebook.dependencies.add(self.mee_go_IVI)
+
+
+    @raises(ValidationError)
+    def test_requirements_dependency_cycle_check_on_save(self):
+        self.add_cycle_dependency()
+        self.mee_go_Notebook.save()
+
+
+    def test_ability_to_avoid_dependency_graph_validation(self):
+        self.add_cycle_dependency()
+        try:
+            self.mee_go_Notebook.save(clean_dependencies=False)
+        except ValidationError, e:
+            self.fail("The exception shouldn't be raised!")
+
+
+    @raises(ValidationError)
+    def test_full_clean_validates_dependencies(self):
+        self.add_cycle_dependency()
+        self.mee_go_Notebook.full_clean()
+
+
+    def test_ability_to_avoid_full_clean_dependencies(self):
+        self.add_cycle_dependency()
+        try:
+            self.mee_go_Notebook.full_clean(clean_dependencies=False)
+        except ValidationError, e:
+            self.fail("The exception shouldn't be raised!")
+
+
+    def test_can_check_dependencies_before_save(self):
+        try:
+            self.mee_go_Notebook.clean_dependencies([self.mee_go_IVI])
+        except ValidationError, e:
+            pass
+        else:
+            self.fail("clean_dependencies method should raise error")
+
+        dependencies = [d.id for d in self.mee_go_Notebook.dependencies.all()]
+        assert_true(self.mee_go_IVI.id not in dependencies)
+
+
+    @raises(ValidationError)
+    def test_trying_to_add_new_dependencies_that_would_make_two_cycles(self):
+        self.mee_go_Notebook.dependencies.add(self.mee_go_IVI)
+        self.mee_go_Notebook.dependencies.add(self.flashlight)
+        self.mee_go_Notebook.save()
+
+
+
+class RequirementFormTest(DependencyTestCase):
+    def configure_form_with_direct_cycle_dependency(self):
+        data = {'dependencies': [self.mee_go.id, self.mee_go_TV.id]}
+        return RequirementForm(data, instance=self.mee_go_Notebook)
+
+    def configure_form_with_dependency_cycle(self):
+        data = {'dependencies': [self.mee_go.id, self.flashlight.id]}
+        return RequirementForm(data, instance=self.mee_go_Notebook)
+
+
+    def test_dependencies_that_make_DIRECT_cycle_are_not_avaiable_for_users_form_select(self):
+        form = self.configure_form_with_direct_cycle_dependency()
+
+        avaiable_dependencies = [r.id for r in form.fields['dependencies'].queryset.all()]
+        assert_true(self.mee_go_TV.id not in avaiable_dependencies)
+
+
+    def test_trying_to_add_direct_cycle_via_form_causes_an_dependencies_validation_error(self):
+        form = self.configure_form_with_direct_cycle_dependency()
+
+        assert_false(form.is_valid())
+        assert_true('dependencies' in form.errors)
+
+        dependencies_errors_list = form.errors['dependencies']
+        assert_equals(len(dependencies_errors_list), 1)
+
+        first_error_msg = dependencies_errors_list[0]
+        assert_true(first_error_msg.startswith('Select a valid choice.'))
+
+
+    def test_cycles_are_discover_and_causes_validation_error(self):
+        form = self.configure_form_with_dependency_cycle()
+
+        assert_false(form.is_valid())
+        assert_true('dependencies' in form.errors)
+
+        dependencies_errors_list = form.errors['dependencies']
+        assert_equals(len(dependencies_errors_list), 1)
+
+        first_error_msg = dependencies_errors_list[0]
+        assert_true(first_error_msg.startswith('You cannot set '))
+
+
+    def new_requirement_form_data(self):
+        return {
+            'parent': self.mee_go_Notebook.id,
+            'dependencies': [self.mee_go.id, self.flashlight.id],
+            'name': 'TestRequirement',
+            }
+
+
+    def configure_form_for_new_requirement_object(self):
+        data = self.new_requirement_form_data()
+        return RequirementForm(data)
+
+
+    def test_new_requirement_form(self):
+        form = self.configure_form_for_new_requirement_object()
+        assert_true(form.is_valid())
+
+        requirement = form.save()
+        assert_equals(len(requirement.dependencies.all()), 2)
