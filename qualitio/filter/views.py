565d9615e340617a3769745d851bdecdf657712b
Kuba Janoszek <kuba.janoszek@gmail.com>
2011-02-24 13:11:07 +0100

    General Filter beta implementation.

--- qualitio/filter/views.py
+++ qualitio/filter/views.py
@@ -1,32 +1,35 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.http import HttpResponseRedirect
 
 import django_tables
 
-from qualitio.filter.filterset import ModelFilterSet
+from qualitio.filter.fields import *
+from qualitio.filter.filter import Filter
 
 
 def filter(request, model=None, exclude=('lft', 'rght', 'tree_id', 'level')):
     Model = model
     fields_to_exclude = exclude
 
-
-    class ModelFilter(ModelFilterSet):
-        class Meta:
-            model = Model
-            exclude = fields_to_exclude
-
-
     class ModelTable(django_tables.ModelTable):
         class Meta:
             model = Model
             exclude = fields_to_exclude
 
+    form_classes = generate_field_forms(Model, exclude=fields_to_exclude)
 
-    filterset = ModelFilter(request.GET or None)
-    table = ModelTable(filterset.qs)
+    generic_filter = Filter(request.GET, form_classes=form_classes)
+    has_control_params, params = generic_filter.build_from_params()
+    if has_control_params:
+        return HttpResponseRedirect('%s?%s' % (request.path, params.urlencode()))
+
+    query = Q() if not generic_filter.is_valid() else generic_filter.construct_Q()
+    table = ModelTable(Model.objects.filter(query))
+
     return render_to_response('filter/filter.html',
-                              {'filterset': filterset,
-                               'table': table,
-                               'app_label': Model._meta.app_label},
+                              {'table': table,
+                               'app_label': Model._meta.app_label,
+                               'filter': generic_filter,
+                               },
                               context_instance=RequestContext(request))
--- qualitio/static/js/base_filter.js
+++ qualitio/static/js/base_filter.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
     $('input[type="submit"], .button').button();
+    $('.control-select').change(function(){
+	$('.filter-form').submit();
+    });
+    $('input[name$="from_date"], input[name$="to_date"]').datepicker({
+	showWeek: true ,
+	dateFormat: DATE_FORMAT
+    });
 });
