90b260d0614b78a4a73d26300ba06d875e48e282
xando (sebastian.pawlus@qualitio.com) <sebastian.pawlus@qualitio.com>
2011-02-24 13:17:39 +0100

    Closes #72. By introducing 'for_parent_unique' Meta option for BasePathModel classes, additional option created also for renaming relation for subchildren 'for_parent_unique'

--- qualitio/core/tests/models.py
+++ qualitio/core/tests/models.py
@@ -4,20 +4,35 @@ from django.core.exceptions import ValidationError
 from django.db.utils import IntegrityError
 
 from qualitio.core.tests.utils import BaseTestCase
-from qualitio.core.tests.testapp.models import File, Directory
+from qualitio.core.tests.testapp.models import File, Directory, FileNotUnique
 
-class BasePathModelSubclassTest(BaseTestCase):
+class FileTest(BaseTestCase):
     def setUp(self):
         self.addTestApps(['core.tests.testapp'])
-        self.root_dir = Directory.objects.create(name="root")
+        self.root = Directory.objects.create(name="root")
+
+    def test_create(self):
+        file = File.objects.create(name="name", parent=self.root)
+        self.assertTrue(file.pk is not None)
 
     @raises(IntegrityError)
-    def test_BasePath_based_object_save_exception_without_parent(self):
-        File.objects.create(name="myFile");
+    def test_create_without_parent(self):
+        File.objects.create(name="name");
 
-    def test_BasePath_based_object_need_to_have_name_and_parent_to_save(self):
-        try:
-            test_file = File.objects.create(name="myFile", parent=self.root_dir)
-        except ValidationError, e:
-            self.fail("The exception shouldn't be raised here.")
+    @raises(ValidationError)
+    def test_unique(self):
+        File.objects.create(parent=self.root, name="name")
+        File.objects.create(parent=self.root, name="name")
 
+    def test_create_not_unique(self):
+        FileNotUnique.objects.create(parent=self.root, name="name")
+        FileNotUnique.objects.create(parent=self.root, name="name")
+
+        self.assertEqual(FileNotUnique.objects.filter(parent=self.root).count(), 2)
+
+    def test_related_name(self):
+        File.objects.create(parent=self.root, name="name")
+        FileNotUnique.objects.create(parent=self.root, name="name")
+
+        self.assertEqual(self.root.unique_files.count(), 1)
+        self.assertEqual(self.root.not_unique_files.count(), 1)
