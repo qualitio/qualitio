08ae10dd6b86ddd4387702c3cf078b408af51c0a
xando (sebastian.pawlus@qualitio.com) <sebastian.pawlus@qualitio.com>
2011-02-28 16:39:16 +0100

    Closes 82

--- qualitio/static/js/main.js
+++ qualitio/static/js/main.js
@@ -102,4 +102,12 @@ $(window).resize(function() {
   resize_main_window();
 });
 
-
+(function( $ ){
+  $.fn.serializeJSON=function() {
+    var json = {};
+    jQuery.map($(this).serializeArray(), function(n, i){
+      json[n['name']] = n['value'];
+    });
+    return json;
+  };
+})( jQuery );
