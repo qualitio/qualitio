99cb39a37c926a23d68d3e8dd6aee8411c94baf6
xando (sebastian.pawlus@qualitio.com) <sebastian.pawlus@qualitio.com>
2011-03-02 11:54:26 +0100

    Table filters removed from requirement details view

--- qualitio/static/js/require_details.js
+++ qualitio/static/js/require_details.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $("table.display").dataTable({
     "sScrollY": "160px",
     "bPaginate": false,
-    "sDom": 'rt<"bottom clearfix"lfp><"clear">',
+    "bFilter": false,
     "sWidth": "4px", "aTargets": [0],
     "aoColumnDefs": [ { "sWidth": "4px", "aTargets": [0,1] } ]
   });
