04eb1c7ddfc920a65237f8f4953498ca98b116dc
xando (sebastian.pawlus@qualitio.com) <sebastian.pawlus@qualitio.com>
2011-03-15 11:06:25 +0100

    Middleware inclusion path fixed

--- qualitio/settings.py
+++ qualitio/settings.py
@@ -69,7 +69,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
-    'core.middleware.LoginRequiredMiddleware',
+    'qualitio.core.middleware.LoginRequiredMiddleware',
     'django.middleware.transaction.TransactionMiddleware',
 )
 
