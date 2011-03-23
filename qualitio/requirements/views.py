d73a83b99903f56213c9d6a98eb7597b5d1fdebe
xando (sebastian.pawlus@qualitio.com) <sebastian.pawlus@qualitio.com>
2011-03-23 12:11:03 +0100

    Redesign of Requirements / test cases attaching view.
    Closes #11.

--- qualitio/requirements/views.py
+++ qualitio/requirements/views.py
@@ -6,7 +6,7 @@ from django.db.models import Q
 from reversion import revision
 
 from qualitio.core.utils import json_response, success, failed
-from qualitio.store.models import TestCase
+from qualitio import store
 from qualitio.requirements.models import Requirement
 from qualitio.requirements.forms import RequirementForm, SearchTestcasesForm
 from qualitio.requirements.tables import RequirementsFilterTable
@@ -52,50 +52,36 @@ def valid(request, requirement_id=0):
 
         requirement = requirement_form.save()
         return success(message='Requirement saved: %s' % revision.comment,
-                       data={ "parent_id" : getattr(requirement.parent,"id", 0),
-                              "current_id" : requirement.id })
+                       data={"parent_id" : getattr(requirement.parent,"id", 0),
+                             "current_id" : requirement.id })
 
     return failed(message="Validation errors %s" % requirement_form.error_message(),
                   data=requirement_form.errors_list())
 
 
-def test_cases(request, requirement_id):
+def testcases(request, requirement_id):
     requirement = Requirement.objects.get(pk=requirement_id)
-    connected_testcases = requirement.testcase_set.all()
-    available_testcases = TestCase.objects.all()
     return render_to_response('requirements/test_cases.html',
-                              {'requirement' : Requirement.objects.get(pk=requirement_id),
-                               'connected_testcases' : connected_testcases,
-                               'available_testcases' : available_testcases,
+                              {'requirement' : requirement,
+                               'connected_testcases' : requirement.testcase_set.all(),
+                               'available_testcases' : store.TestCase.objects.all(),
                                'search_testcases_form' : SearchTestcasesForm()},
                               context_instance=RequestContext(request))
 
-@json_response
-def available_testcases(request, requirement_id):
-    search_testcases_form = SearchTestcasesForm(request.POST)
-    if search_testcases_form.is_valid():
-        search = request.POST["search"]
-        testcases =  TestCase.objects.filter(Q(name__contains=search) | Q(path__contains=search))
-        if testcases:
-            return success(message="%s testcases found" % testcases.count(),
-                           data=loader.render_to_string("requirements/_available_testcases.html",
-                                                        { "testcases" : testcases }))
-        return success(message="no testcases found")
-
-    return failed(message="validation errors",
-                  data=search_testcases_form.errors_list())
-
 
 @json_response
-def connect_testcases(request, requirement_id):
-    requirement = Requirement.objects.get(id=requirement_id)
-    for testcase in TestCase.objects.filter(id__in=request.POST.getlist("connect")):
-        testcase.requirement = requirement
-        testcase.save()
-    for testcase in TestCase.objects.filter(id__in=request.POST.getlist("disconnect")):
-        testcase.requirement = None
-        testcase.save()
-    return success();
+def testcases_connect(request, requirement_id):
+    requirement = Requirement.objects.get(pk=requirement_id)
+
+    requirement.testcase_set.clear()
+
+    testcases =\
+        store.TestCase.objects.filter(pk__in=request.POST.getlist("connected_test_case"))
+
+    for testcase in testcases:
+        requirement.testcase_set.add(testcase)
+
+    return success(message="Test cases connected")
 
 
 def history(request, requirement_id):
---
+++ qualitio/static/js/require_testcases.js
@@ -0,0 +1,85 @@
+jQuery.fn.refresh = function () {
+  _this = $(this).find("table");
+  
+  _this.find("tr")
+    .removeClass("disable")
+    .find("input")
+    .attr("disabled", false);
+
+  $(".connected-testcases table").find("input[name=connected_test_case]").each( function(i, el) {
+    origin_tr_element = _this.find("input[name=available_test_case][value="+ $(this).val() +"]").parents("tr");
+    origin_tr_element 
+      .addClass("disable")
+      .find("input")
+      .attr("disabled", true)
+      .attr("checked", false);
+  });
+  
+  $(this).find("input[name=select-all]").attr("checked", false);
+}
+
+$(function() {
+  var connected_testcases = $(".connected-testcases table.display").dataTable({
+    "sScrollY": "300px",
+    "bPaginate": false,
+    "bFilter": false,
+    "sDom": 'rt<"bottom clearfix"lfp><"clear">',
+    "aoColumnDefs": [
+      { "bSortable": false, "aTargets": [0],
+        "sWidth": "4px", "aTargets": [0]}
+    ]
+  });
+  $("#remove-testcases-button").appendTo($(".connected-testcases .bottom"));
+
+  var available_testcases = $(".available-testcases table.display").dataTable({
+    "sScrollY": "300px",
+    "bPaginate": false,
+    "sDom": 'rt<"bottom clearfix"lfp><"clear">',
+    "aoColumnDefs": [
+      { "bSortable": false, "aTargets": [0],
+        "sWidth": "4px", "aTargets": [0]}
+    ]
+  });
+  $("#add-testcases-button").appendTo($(".available-testcases .bottom"));
+
+  $('input[name=select-all]').dataTableToggleSelect();
+
+  $(".available-testcases").refresh();
+
+  $("#remove-testcases-button").click(function() {
+    $(".connected-testcases input.modify:checked").each( function(i) {
+      var pos = connected_testcases.fnGetPosition( $(this).parents('tr')[0] );
+      connected_testcases.fnDeleteRow(pos);
+    });
+    $(".connected-testcases input[name=select-all]").attr("checked", false);
+    $(".available-testcases").refresh();
+  });
+
+  $("#add-testcases-button").click( function() {
+    $(".available-testcases input.modify:checked").each( function() {
+      parent_tr = $(this).parents("tr");
+      
+      control_box = parent_tr.find('.select').clone();
+      control_box.find('input[name=available_test_case]').attr("name", "connected_test_case");
+
+      connected_testcases.fnAddData([control_box.html(),
+                                     parent_tr.find('.id').html(),
+                                     parent_tr.find('.path').html(),
+                                     parent_tr.find('.name').html(),
+                                     parent_tr.find('.modified_time').html(),
+                                     parent_tr.find('.created_time').html()]);
+    });
+    $(".available-testcases").refresh();
+  });
+  
+  $('#testcases_connect_form').ajaxForm({ 
+    success: function(response, statusText, xhr, $form) {
+      $('#notification').jnotifyAddMessage({
+        text: response.message,
+        permanent: false,
+        disappearTime: 2000
+      });
+    },
+  });
+
+});
