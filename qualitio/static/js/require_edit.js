977df86968305981680005e9619112fa6842fb99
xando (sebastian.pawlus@qualitio.com) <sebastian.pawlus@qualitio.com>
2011-03-25 13:22:04 +0100

    Edit view: after save stays in the same location.

--- qualitio/static/js/require_edit.js
+++ qualitio/static/js/require_edit.js
@@ -16,9 +16,6 @@ function show_response(response, statusText, xhr, $form)  {
       permanent: false,
       disappearTime: 2000
     });
-    hash.node = response.data.current_id; // for new created objects go to details view 
-    hash.view = "details" 
-    hash.update();
     $('#application-tree').jstree('refresh', "#"+response.data.parent_id+"_requirement", response.data);
     
     $('#application-tree').bind("refresh.jstree", function (event, data) {
