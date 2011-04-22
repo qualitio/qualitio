8bee60edf3817a26f914e386b9b81810ed6372cd
xando (sebastian.pawlus@qualitio.com) <sebastian.pawlus@qualitio.com>
2011-04-22 14:02:32 +0200

    Local settings deployment fabric function.

--- fabfile.py
+++ fabfile.py
@@ -39,7 +39,7 @@ def setup_development():
                   colors.green("%s/.virtualenv" % os.getcwd()) + " directory")
 
 
-def setup_production(path="/var/www/qualitio"):
+def setup_production(path="/var/www/qualitio", local_settings=""):
     "Creates remote production envirotment"
 
     #TODO: switch to reall check. This normalization is pretty odd
@@ -56,6 +56,7 @@ def setup_production(path="/var/www/qualitio"):
     _download_release()
     _install_requirements()
     _configure_webserver()
+    _local_settings(local_settings)
     _synchronize_database()
     _restart_webserver()
     _load_dumpdata()
@@ -107,6 +108,18 @@ def _configure_webserver():
     sudo("cp %(path)s/deploy/apache.virtualhost /etc/apache2/sites-available/qualitio" % env)
 
     sudo("a2ensite qualitio")
+
+
+def local_settings(local_settings):
+    require("path")
+
+    if not local_settings:
+        print(colors.yellow("No local settins files provied, using defaults."))
+    else:
+        try:
+            put(settings, "%(path)s/qualitio/local_settings.py" % env, use_sudo=True)
+        except ValueError:
+            print(colors.yellow("Local settings file doesn't exists, using defaults."))
 
 
 def _synchronize_database():
