cda8bcc1c81cdc7a20d952cd4768b070b835b2ce
xando (sebastian.pawlus@qualitio.com) <sebastian.pawlus@qualitio.com>
2011-05-04 14:28:01 +0200

    Hash controller is no longer needed in this form.

--- qualitio/static/js/main.js
+++ qualitio/static/js/main.js
@@ -1,86 +1,3 @@
-// HashController,
-// Implementation based on jquery-ui site, try to understand it, don't blame it.
-var hash = {
-  storedHash: '',
-  listen: true,
-  interval: null,
-  cache: '',
-
-  object : null,
-  node : null,
-  view : null,
-
-  _start_listening: function() {
-    setTimeout(function() {
-      hash.listen = true;
-    }, 600);
-  },
-
-  _stop_listening: function() {
-    hash.listen = false;
-  },
-
-  _has_changed: function() {
-    var locStr = hash.current_hash();
-    if(hash._clean(hash.storedHash) != locStr) {
-      hash._parse();
-
-      if(hash.listen == true) {
-        hash.main();
-        hash.post_main();
-      }
-      hash.storedHash = locStr;
-    }
-    if(!hash.interval) hash.interval = setInterval(hash._has_changed, 50);
-  },
-
-  _parse: function() {
-    segments = hash.current_hash().split("/");
-    hash.object = segments[0];
-    hash.node = segments[1];
-    hash.view = segments[2];
-  },
-
-  _clean: function(locStr){
-    return locStr.replace(/%23/g, "").replace(/[\?#]+/g, "");
-  },
-
-  init: function(){
-    if (window.location.hash) {
-      hash._parse()
-      hash.update();
-    } else {
-      hash.storedHash = '';
-    }
-    hash._has_changed();
-  },
-
-  update: function(refresh) {
-    hash.storedHash = hash.to_string();
-    window.location.hash = hash.to_string();
-    if (refresh) {
-      hash.main();
-    }
-  },
-
-  current_hash: function() {
-    return hash._clean(window.location.hash);
-  },
-
-  to_string: function() {
-    return "#"+[hash.object, hash.node, hash.view].join("/")+"/";
-  },
-
-  // Controller view. Will react on every anchor change. Implement your logic here
-  main: function() {
-    return alert("One function to rule them all, not implemented")
-  },
-
-  post_main: function() {
-    return 0;
-  }
-};
-
 var resize_main_window = function() {
   $('#application-view')
     .css('height', 
@@ -159,9 +76,11 @@ jQuery.shortcuts = {
   },
 
   selectTreeNode: function(id, type) {
-    jQuery.getJSON('ajax/get_antecedents', {'id': id, 'type': type}, function(data) {
-      jQuery.shortcuts._openNode(data.nodes, data.target);
-    });
+    if ( !$('#application-tree').jstree("is_selected", "#"+id+"_"+type) && id) {
+      jQuery.getJSON('ajax/get_antecedents', {'id': id, 'type': type}, function(data) {
+        jQuery.shortcuts._openNode(data.nodes, data.target);
+      });
+    }
   }
 }
 
