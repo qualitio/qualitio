f7ed4972e854f782f075d8c5764e626da981d199
xando <sebastian.pawlus@gmail.com>
2011-07-13 11:55:33 +0200

    blank chars removed

--- qualitio/static/js/require.js
+++ qualitio/static/js/require.js
@@ -1,6 +1,6 @@
 $(function() {
   var ControllerView = Backbone.Controller.extend({
-    
+
     routes: {
       ":type/:id/:view/": "render",
     },
@@ -10,15 +10,15 @@ $(function() {
       this.application_tree = new ApplicationTree({"directory": "requirement",
                                                    "directory_icon": "/static/images/tree/requirement.png"});
     },
-    
+
     render: function(type, id, view) {
       this.application_tree.update(type, id, view);
       this.application_view.render(type, id, view);
       $.shortcuts.selectTreeNode(id, type);
     },
-    
+
   });
-  
+
   new ControllerView();
   Backbone.history.start();
 });
