0a19938b357b7e001170d29bef3e889b64a730a6
Kuba Janoszek <kuba.janoszek@gmail.com>
2011-07-27 15:32:03 +0200

    Clone added for testcases. Closes #106

--- qualitio/static/js/store_testcase_details.js
+++ qualitio/static/js/store_testcase_details.js
@@ -1,3 +1,10 @@
 $(function() {
   $(".glossary-language-switch").languageSwitcher();
+
+  $('input[value=Clone]').click(function() {
+    jQuery.get('ajax/testcase/' + $(this).attr('name') + '/copy/', function(response){
+      $.notification.notice(response.message);
+      $.shortcuts.reloadTree(response.data, "testcasedirectory", "testcase", response.data.current_id);
+    });
+  });
 });
