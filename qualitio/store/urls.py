514867c34972ff81a83d69cf6f7b6494ca6083e9
Kuba Janoszek <kuba.janoszek@gmail.com>
2011-08-05 15:23:35 +0200

    filter app reactivated.

--- qualitio/store/urls.py
+++ qualitio/store/urls.py
@@ -13,7 +13,7 @@ urlpatterns = patterns('',
                            {'model': TestCase,
                             'fields_order': ['id', 'path', 'name', 'requirement'],
                             'exclude': ['lft', 'rght', 'tree_id', 'level', 'precondition', 'description', 'parent'],
-                            'app_menu_items': [{'name': 'glossary', 'url': '/glossary/'}],
+                            'app_menu_items': [{'name': 'glossary', 'url': 'glossary/'}],
                             }),
 
                        url(r'^ajax/get_children$', core.get_children,
