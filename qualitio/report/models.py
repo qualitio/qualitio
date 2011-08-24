c6e7e75010b5ed0d86a08def71c89af3ebcc3b4f
Kuba Janoszek <kuba.janoszek@gmail.com>
2011-08-24 13:16:43 +0200

    Extra variables added to report context. Please see issue #256 for more details.

--- qualitio/report/models.py
+++ qualitio/report/models.py
@@ -47,10 +47,21 @@ class Report(core.BasePathModel):
            context_dict[context_element.name] = context_element.build(self.bound_id)
         return context_dict
 
-    @property
-    def content(self):
+    def content(self, request=None):
         template = Template(self.template)
         context = Context(self.context_dict)
+
+        if request:
+            context.update({
+                    'self': {
+                        'user': {
+                            'name': request.user.username,
+                            'first_name': request.user.first_name,
+                            'last_name': request.user.last_name,
+                            'email': request.user.email,
+                            }
+                        }})
+
         return template.render(context)
 
     def is_html(self):
