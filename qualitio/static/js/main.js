ad55a68eb107c578888e0d8402450b1129fc3ee8
Kuba Janoszek <kuba.janoszek@gmail.com>
2011-08-24 12:37:40 +0200

    JS shortcuts.showErrors functions fixed a little. It doesn't show errors for fields marked as "removed".

--- qualitio/static/js/main.js
+++ qualitio/static/js/main.js
@@ -45,15 +45,18 @@ $(window).resize(function() {
 jQuery.shortcuts = {
   showErrors: function(errors) {
     $(errors).each(function(i, element, value) {
-      field = element[0]; message = element[1];
+      var field = element[0], message = element[1];
 
-      $field = $('#id_'+ field);
-      $field_errors = $('#id_' +field+ '_error');
+      var $field = $('#id_'+ field);
+      var $field_errors = $('#id_' +field+ '_error');
+      var parentIsRemoved = $field.parents('.removed').length > 0;
 
-      if( $field_errors.length ) {
-        $field_errors.text(message).fadeIn();
-      } else {
-        $field.before($('<div style="display:block" class="error">'+message+'</div>').fadeIn());
+      if (! parentIsRemoved) {  // we don't want to validate removed / deleted items
+	if( $field_errors.length ) {
+          $field_errors.text(message).fadeIn();
+	} else {
+          $field.before($('<div style="display:block" class="error">'+message+'</div>').fadeIn());
+	}
       }
     });
   },
