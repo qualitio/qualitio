fbec0b2376468821ce6164ee83ee0b184e3ffe90
Kuba Janoszek <kuba.janoszek@gmail.com>
2011-09-09 14:03:18 +0200

    Permissions for filter & actions added.

--- qualitio/actions/urls.py
+++ qualitio/actions/urls.py
@@ -4,7 +4,10 @@ from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
+from qualitio.projects.auth.decorators import permission_required
+from qualitio.actions.views import actions
+
 urlpatterns = patterns('',
                        url(r'^action/execute/(?P<app_label>require|store|execute)/(?P<action_name>\w+)/$',
-                           'qualitio.actions.views.actions', name="qualitio-actions"),
+                           permission_required('USER')(actions), name="qualitio-actions"),
                        )
