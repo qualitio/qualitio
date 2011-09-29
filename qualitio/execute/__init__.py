3b95bef1e2b92268b6d0f0703116e562323ec366
xando <sebastian.pawlus@gmail.com>
2011-09-29 12:10:13 +0200

    Reversion code live again damn it. Stupid lib.

--- qualitio/execute/__init__.py
+++ qualitio/execute/__init__.py
@@ -3,13 +3,13 @@ from qualitio import module_absolute_url
 from qualitio.execute.models import TestRunDirectory, TestRun, TestRunStatus, TestCaseRun, Bug, TestCaseRunStatus
 
 
-# if not reversion.is_registered(TestRun):
-#     reversion.register(TestRun, follow=["testcaserun_set"])
-#     reversion.register(TestCaseRun, follow=["parent", "bugs"])
-#     reversion.register(Bug, ["testcaserun"])
+if not reversion.is_registered(TestRun):
+    reversion.register(TestRun, follow=["testcaserun_set"])
+    reversion.register(TestCaseRun, follow=["parent", "bugs"])
+    reversion.register(Bug, ["testcaserun"])
 
-# if not reversion.is_registered(TestRunDirectory):
-#     reversion.register(TestRunDirectory)
+if not reversion.is_registered(TestRunDirectory):
+    reversion.register(TestRunDirectory)
 
 
 get_absolute_url = module_absolute_url(module_name="execute")
