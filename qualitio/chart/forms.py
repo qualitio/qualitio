798838e252ce9cb22a7822500b091af524011e8f
Kuba Janoszek <kuba@kuba.janoszek>
2011-12-13 13:02:41 +0100

    chart forms: unused forms removed

--- qualitio/chart/forms.py
+++ qualitio/chart/forms.py
@@ -1,34 +1,7 @@
 # -*- coding: utf-8 -*-
-from operator import attrgetter
-
 from django import forms
 
-from qualitio.require.models import Requirement
-from qualitio.store.models import TestCase
-from qualitio.execute.models import TestCaseRun, Bug
 from qualitio.chart.models import charttypes
-
-avaiable_models = (
-    ("requirements", "requirements"),
-    ("testcases", "testcases"),
-    ("testcaseruns", "testcase runs"),
-    ("bugs", "bugs"),
-    )
-
-models = {
-    "requirements": Requirement,
-    "testcases": TestCase,
-    "testcaseruns": TestCaseRun,
-    "bugs": Bug,
-    }
-
-class ChartModelChoiceForm(forms.Form):
-    xaxis = forms.ChoiceField(choices=avaiable_models)
-    yaxis = forms.ChoiceField(choices=avaiable_models)
-
-    def get_models(self):
-        cd = self.cleaned_data
-        return models[cd['xaxis']], models[cd['yaxis']]
 
 
 class ChartTypeChoiceForm(forms.Form):
