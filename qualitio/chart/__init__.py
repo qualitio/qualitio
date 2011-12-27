5868e650696591dc4cb6f226afa3975b9fa3ea01
Kuba Janoszek <kuba@kuba.janoszek>
2011-12-27 15:27:39 +0100

    "chart" tag added to buildins tags.

--- qualitio/chart/__init__.py
+++ qualitio/chart/__init__.py
@@ -2,3 +2,6 @@ from qualitio import module_absolute_url
 
 get_absolute_url = module_absolute_url(module_name="chart")
 verbose_name = "chart"
+
+from django.template.base import add_to_builtins
+add_to_builtins('qualitio.chart.templatetags.charts')
