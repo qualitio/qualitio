bf4e6812084fd40cd4922a650e5a9dda9dc98800
zulawin <jacek.zulawinski@qualitio.com>
2012-01-23 15:44:14 +0100

    fixed of visibility password confirmation on registration page for new user

--- qualitio/static/js/login.js
+++ qualitio/static/js/login.js
@@ -4,6 +4,10 @@ $(function() {
     $("label").inFieldLabels();
     $("input").attr("autocomplete","off");
 
+/* Commented,  fixed of visibility password confirmation on registration page for new user
+   new problem, on page of creation organization for returned user visible password confirmation
+   ( to improve in the future ) 
+
     var $password = $("#id_password2, label[for=id_password2]");
 
     $password.toggle(
@@ -15,7 +19,9 @@ $(function() {
             $(".user-type input:checked").val() == "1"
         );
     });
+*/
 })
+
 
 $(window).resize(function(){
   $("label").inFieldLabels();
