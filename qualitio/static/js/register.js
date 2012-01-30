07ff170a6aa09fc1bd5293a4b073f05729984c56
Milosz Wasilewski <milosz.wasilewski@qualitio.com>
2012-01-31 00:46:14 +0200

    fixed problem with missing second password field in registration template

---
+++ qualitio/static/js/register.js
@@ -0,0 +1,18 @@
+$(function() {
+    $('input[type=submit], .button').button();
+
+    $("label").inFieldLabels();
+    $("input").attr("autocomplete","off");
+
+    var $password = $("#id_password2, label[for=id_password2]");
+
+    $(".user-type input").change(function() {
+        $password.toggle(
+            $(".user-type input:checked").val() == "1"
+        );
+    });
+})
+
+$(window).resize(function(){
+  $("label").inFieldLabels();
+});
